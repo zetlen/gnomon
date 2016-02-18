7bbd90366cdf30023dc5fc635bd685828dd75256
James Zetlen <jzetlen@paypal.com>
2016-02-18 12:49:22 -0600

    v2, better options and elapsed time

---
+++ bin/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2), {
+  string: ['format','type'],
+  boolean: ['ignore-blank', 'ignoreBlank'],
+  alias: {
+    'format': 'f',
+    'type': 't',
+    'ignoreBlank': ['ignore-blank','quiet','q'],
+    'high': 'h',
+    'medium': 'm'
+  }
+});
+var split = require('split');
+var gnomon = require('../');
+process.stdin.pipe(split())
+.pipe(gnomon(argv))
+.pipe(process.stdout);
--- index.js
+++ index.js
@@ -1,21 +1,90 @@
-#!/usr/bin/env node
 var dateutil = require('dateutil');
-var split = require('split');
 var chalk = require('chalk');
 var through = require('through');
-var argv = require('minimist')(process.argv.slice(2));
 
-var fmt = argv._.length === 0 ? '[H:i:s.u O]' : argv._[0];
+module.exports = function(opts) {
+  var fmt = opts.format || 'H:i:s.u O';
+  var zeroPads = ['0','00','000'];
+  var start = (new Date()).getTime();
+  var last = start;
+  var now = start;
+  var elapsed = 0;
+  function tick() {
+    now = (new Date()).getTime();
+    elapsed = now - last;
+    last = now;
+  }
+  function formatDuration(duration) {
+    var s = (duration / 1000).toFixed(3);
+    var l = s.length;
+    var pad = l < 8 ? zeroPads[7 - l] : '';
+    return pad + s;
+  }
+  function formatStamp(stamp) {
+    return '[' + stamp + ']';
+  }
+  function stampLine(stamp, line) {
+    return stamp + ' ' + line + '\n';
+  }
 
-var createDate = function() {
-  return dateutil.format(new Date(), fmt);
+  var createStampTime = ({
+    'elapsed-line': function() {
+      return formatStamp(formatDuration(elapsed));
+    },
+    'elapsed-total': function() {
+      return formatStamp(formatDuration(now - start));
+    },
+    'absolute': function() {
+      return formatStamp(dateutil.format(new Date(), fmt));
+    }
+  })[opts.type || 'elapsed-line'];
+
+  var createStamp;
+  if (opts.medium && opts.high) {
+    createStamp = function() {
+      if (elapsed >= opts.high) {
+        return chalk.reset.red(createStampTime());
+      }
+      if (elapsed >= opts.medium) {
+        return chalk.reset.yellow(createStampTime());
+      }
+      return chalk.reset.green(createStampTime());
+    };
+  } else if (opts.medium) {
+    createStamp = function() {
+      if (elapsed >= opts.medium) {
+        return chalk.reset.yellow(createStampTime());
+      }
+      return chalk.reset.green(createStampTime());
+    }
+  } else if (opts.high) {
+    createStamp = function() {
+      if (elapsed >= opts.high) {
+        return chalk.reset.red(createStampTime());
+      }
+      return chalk.reset.green(createStampTime());
+    }
+  } else {
+    createStamp = function() {
+      return chalk.reset(createStampTime());
+    }
+  }
+
+  var lastLine = false;
+
+  var feed = function(stream) {
+    stream.queue(stampLine(createStamp(), lastLine));
+  }
+
+  var write = function (stream, data) {
+    tick();
+    if (lastLine !== false) feed(stream);
+    lastLine = data;
+  }
+
+  var onData = (opts.ignoreBlank)
+    ? function (data) { data ? write(this, data) : this.queue('\n'); }
+    : function (data) { write(this, data); };
+
+  return through(onData, function end () { feed(this); });
 };
-
-var createStamp = (argv.color === false || argv['no-color'])
-  ? createDate : function() { return chalk.yellow(createDate()); }
-
-process.stdin.pipe(split())
-.pipe(through(function write (data) {
-  this.queue(createStamp() + ' ' + data + '\n');;
-}))
-.pipe(process.stdout);
